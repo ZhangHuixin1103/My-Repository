861357d9f5f11c3efd5ecc620b8945e0981d2d98
Zhang Huixin <rzxyxxzhx1103@gmail.com>
2022-10-02 23:21:44 -0500

    Update page_table.C

--- page_table.C
+++ page_table.C
@@ -4,7 +4,7 @@
 #include "paging_low.H"
 #include "page_table.H"
 
-#define PAGE_DIRECTORY_FRAME_SIZE 1
+#define PAGE_DIR_FRAME_SIZE 1
 
 #define PAGE_PRESENT 1
 #define PAGE_WRITE 2
@@ -12,8 +12,7 @@
 
 #define PD_SHIFT 22
 #define PT_SHIFT 12
-
-#define PDE_MASK 0xFFFFF000
+#define PD_MASK 0xFFFFF000
 #define PT_MASK 0x3FF
 
 PageTable *PageTable::current_page_table = NULL;
@@ -35,25 +34,25 @@ void PageTable::init_paging(ContFramePool *_kernel_mem_pool,
 
 PageTable::PageTable()
 {
-    page_directory = (unsigned long *)(kernel_mem_pool->get_frames(PAGE_DIRECTORY_FRAME_SIZE) * PAGE_SIZE);
+    page_directory = (unsigned long *)(kernel_mem_pool->get_frames(PAGE_DIR_FRAME_SIZE) * PAGE_SIZE);
 
-    unsigned long mask_addr = 0;
-    unsigned long *dir_map_page_table = (unsigned long *)(kernel_mem_pool->get_frames(PAGE_DIRECTORY_FRAME_SIZE) * PAGE_SIZE);
+    unsigned long mask_address = 0;
+    unsigned long *dir_map_page_table = (unsigned long *)(kernel_mem_pool->get_frames(PAGE_DIR_FRAME_SIZE) * PAGE_SIZE);
     unsigned long shared_frames = (PageTable::shared_size / PAGE_SIZE);
 
     for (int i = 0; i < shared_frames; i++)
     {
-        dir_map_page_table[i] = mask_addr | PAGE_WRITE | PAGE_PRESENT;
-        mask_addr += PAGE_SIZE;
+        dir_map_page_table[i] = mask_address | PAGE_WRITE | PAGE_PRESENT;
+        mask_address += PAGE_SIZE;
     }
 
     page_directory[0] = (unsigned long)dir_map_page_table | PAGE_WRITE | PAGE_PRESENT;
 
-    mask_addr = 0;
+    mask_address = 0;
 
     for (int i = 1; i < shared_frames; i++)
     {
-        page_directory[i] = mask_addr | PAGE_WRITE;
+        page_directory[i] = mask_address | PAGE_WRITE;
     }
 
     Console::puts("Constructed Page Table object\n");
@@ -79,34 +78,34 @@ void PageTable::handle_fault(REGS *_r)
 {
     unsigned long *cur_page_directory = (unsigned long *)read_cr3();
 
-    unsigned long page_addr = read_cr2();
-    unsigned long pd_addr = page_addr >> PD_SHIFT;
-    unsigned long pt_addr = page_addr >> PT_SHIFT;
+    unsigned long page_address = read_cr2();
+    unsigned long pd_address = page_address >> PD_SHIFT;
+    unsigned long pt_address = page_address >> PT_SHIFT;
 
     unsigned long *page_table = NULL;
     unsigned long error_code = _r->err_code;
 
-    unsigned long mask_addr = 0;
+    unsigned long mask_address = 0;
 
     if ((error_code & PAGE_PRESENT) == 0)
     {
-        if ((cur_page_directory[pd_addr] & PAGE_PRESENT) == 1)
+        if ((cur_page_directory[pd_address] & PAGE_PRESENT) == 1)
         {
-            page_table = (unsigned long *)(cur_page_directory[pd_addr] & PDE_MASK);
-            page_table[pt_addr & PT_MASK] = (PageTable::process_mem_pool->get_frames(PAGE_DIRECTORY_FRAME_SIZE) * PAGE_SIZE) | PAGE_WRITE | PAGE_PRESENT;
+            page_table = (unsigned long *)(cur_page_directory[pd_address] & PD_MASK);
+            page_table[pt_address & PT_MASK] = (PageTable::process_mem_pool->get_frames(PAGE_DIR_FRAME_SIZE) * PAGE_SIZE) | PAGE_WRITE | PAGE_PRESENT;
         }
         else
         {
-            cur_page_directory[pd_addr] = (unsigned long)((kernel_mem_pool->get_frames(PAGE_DIRECTORY_FRAME_SIZE) * PAGE_SIZE) | PAGE_WRITE | PAGE_PRESENT);
+            cur_page_directory[pd_address] = (unsigned long)((kernel_mem_pool->get_frames(PAGE_DIR_FRAME_SIZE) * PAGE_SIZE) | PAGE_WRITE | PAGE_PRESENT);
 
-            page_table = (unsigned long *)(cur_page_directory[pd_addr] & PDE_MASK);
+            page_table = (unsigned long *)(cur_page_directory[pd_address] & PD_MASK);
 
             for (int i = 0; i < 1024; i++)
             {
-                page_table[i] = mask_addr | PAGE_LEVEL;
+                page_table[i] = mask_address | PAGE_LEVEL;
             }
 
-            page_table[pt_addr & PT_MASK] = (PageTable::process_mem_pool->get_frames(PAGE_DIRECTORY_FRAME_SIZE) * PAGE_SIZE) | PAGE_WRITE | PAGE_PRESENT;
+            page_table[pt_address & PT_MASK] = (PageTable::process_mem_pool->get_frames(PAGE_DIR_FRAME_SIZE) * PAGE_SIZE) | PAGE_WRITE | PAGE_PRESENT;
         }
     }
 
